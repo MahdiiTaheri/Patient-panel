7f1225783b81435aa9c561a5fe7782ee136d42fd
Mahdi taheri <m.taheri123456789@gmail.com>
2024-06-12 19:24:02 +0330

    creating add patients button

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,27 +1,190 @@
-import PatientsForm from "./PatientsForm";
-import { Button } from "./ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { Dispatch, SetStateAction } from "react";
 
-function AddButton() {
+const StatusTypeSchema = z.enum(["پرداخت نشده", "ترخیص شده", "بستری"], {
+  message: "وضعیت بیمار باید بستری، ترخیص شده و یا پرداخت نشده باشد",
+  required_error: "این فیلد باید پر شود",
+});
+export const formSchema = z.object({
+  id: z
+    .number({
+      invalid_type_error: "آیدی باید عدد باشد",
+    })
+    .min(1, { message: "آیدی نباید خالی باشد" }),
+  name: z
+    .string()
+    .min(3, { message: "نام بیمار نباید کمتر از 3 حرف باشد" })
+    .max(30),
+  payment: z.preprocess(
+    (a) => parseInt(z.string().parse(a), 10),
+    z.number({ invalid_type_error: "هزینه باید عدد باشد" }).positive().min(1)
+  ),
+  status: StatusTypeSchema,
+});
+
+interface AddButtonProps {
+  onSubmit: (values: z.infer<typeof formSchema>) => void;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+function AddButton({ onSubmit, open, setOpen }: AddButtonProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      id: 0,
+      name: "",
+      payment: 0,
+      status: "بستری",
+    },
+  });
+
   return (
     <div>
-      <Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger className="bg-cyan-700 text-gray-100 rounded-xl p-4 mx-6 mb-6">
           اضافه کردن بیمار جدید
         </DialogTrigger>
         <DialogContent>
-          <DialogHeader>
-            <DialogDescription>
-              <PatientsForm />
-            </DialogDescription>
-          </DialogHeader>
+          <Card className=" min-w-[350px] lg:w-full border-none">
+            <DialogHeader>
+              <CardTitle className="font-semibold text-lg text-center">
+                مشخصات بیمار جدید
+              </CardTitle>
+            </DialogHeader>
+
+            <CardContent>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="id"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel dir="">آیدی</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="17"
+                            className="text-md"
+                            dir="rtl"
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>نام بیمار</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="علی علوی"
+                            {...field}
+                            className="text-md"
+                            dir="rtl"
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="status"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>وضعیت</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="ترخیص شده"
+                            {...field}
+                            className="text-md"
+                            dir="rtl"
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="payment"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>هزینه</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="10000 تومان"
+                            {...field}
+                            className="text-md"
+                            dir="rtl"
+                            // type="number"
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <DialogFooter>
+                    <div className="flex justify-between w-full">
+                      <DialogClose asChild>
+                        {/* <div className="flex gap-8 justify-between "> */}
+                        <Button
+                          className="text-lg "
+                          variant="ghost"
+                          type="submit"
+                          size="lg"
+                        >
+                          بازگشت
+                        </Button>
+                        {/* </div> */}
+                      </DialogClose>
+                      <Button
+                        className="text-lg bg-cyan-800"
+                        type="submit"
+                        size="lg"
+                      >
+                        ثبت
+                      </Button>
+                    </div>
+                  </DialogFooter>
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
         </DialogContent>
       </Dialog>
     </div>
--- components/PatientTable.tsx
+++ components/PatientTable.tsx
@@ -11,114 +11,13 @@ import {
 import { ScrollArea } from "./ui/scrollArea";
 import { cn } from "@/lib/utils";
 import Options from "./Options";
+import { Patient } from "@/app/dashboard/patients/page";
 
-interface Patients {
-  id: string;
-  name: string;
-  payment: string;
-  status: string;
+interface PatientTableProps {
+  patients: Patient[];
 }
 
-export const patients: Patients[] = [
-  {
-    id: "01",
-    name: "علی علوی",
-    payment: "1000000",
-    status: "بستری",
-  },
-  {
-    id: "02",
-    name: "مریم مریمی",
-    payment: "2000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "03",
-    name: "رضا رضایی",
-    payment: "1000000",
-    status: "بستری",
-  },
-  {
-    id: "04",
-    name: "محمد محمدی",
-    payment: "4000000",
-    status: "بستری",
-  },
-  {
-    id: "05",
-    name: "علی رضایی",
-    payment: "1000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "06",
-    name: "محمد علوی",
-    payment: "1000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "07",
-    name: "کیانوش رضایی",
-    payment: "1000000",
-    status: "پرداخت نشده",
-  },
-  {
-    id: "08",
-    name: "مهدی مهدوی",
-    payment: "15000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "09",
-    name: "محمد مهدوی",
-    payment: "10000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "10",
-    name: "رضا کیانی",
-    payment: "1000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "11",
-    name: "فرامز رضایی",
-    payment: "1000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "12",
-    name: "کیارش عیاری",
-    payment: "1000000",
-    status: "بستری",
-  },
-  {
-    id: "13",
-    name: "مهدی محمدی",
-    payment: "4000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "14",
-    name: "مهدی رضایی",
-    payment: "1500000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "15",
-    name: "رضا تهرانی",
-    payment: "1000000",
-    status: "ترخیص شده",
-  },
-  {
-    id: "16",
-    name: "محمد همدانی",
-    payment: "1300000",
-    status: "بستری",
-  },
-];
-
-export default function PatientTable() {
+export default function PatientTable({ patients }: PatientTableProps) {
   const paymentSum = patients.reduce(
     (acc, { payment }) => acc + Number(payment),
     0
